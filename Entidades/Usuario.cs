51a00a1540d50daad8916800b7063a3a604d2abf
Andres Orellano <andresorellano591@gmail.com>
2024-05-05 16:43:43 -0500

    Agregado clase usuario en la capa entidades

---
+++ Entidades/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    internal class Usuario
+    {
+        public string NombreUsuario { get; set; }
+        public string Contraseña { get; set; }
+
+        // Constructor
+        public Usuario(string cliente, string contraseña)
+        {
+            NombreUsuario = cliente;
+            Contraseña = contraseña;
+        }
+
+        // Método para validar las credenciales
+        public bool ValidarCredenciales(string cliente, string contraseña)
+        {
+            return NombreUsuario == cliente && Contraseña == contraseña;
+        }
+    }
+}
