1da0e7b2d4ffdfe1058bd8bc1c54eb17d6811653
Andres Orellano <andresorellano591@gmail.com>
2024-05-05 20:01:24 -0500

    Agregado Factura-Producto y pedido, pero con su get y set y de mas.[SANTIAGO MASCO]

--- Entidades/Factura.cs
+++ Entidades/Factura.cs
@@ -8,5 +8,27 @@ namespace Entidades
 {
     internal class Factura
     {
+        public Pedido Pedido { get; set; }
+        public double Total { get; set; }
+        public string CedulaCliente { get; set; } 
+
+        public Factura(Pedido pedido, string cedulaCliente)
+        {
+            Pedido = pedido;
+            Total = pedido.CalcularTotal();
+            CedulaCliente = cedulaCliente;
+        }
+
+        public void ImprimirFactura()
+        {
+            Console.WriteLine("Factura:");
+            Console.WriteLine($"Cédula del Cliente: {CedulaCliente}");
+            foreach (var producto in Pedido.Productos)
+            {
+                Console.WriteLine($"Nombre: {producto.Nombre}, Valor: {producto.Valor}");
+            }
+            Console.WriteLine($"Total: {Total}");
+        }
     }
 }
+
---
+++ Entidades/Pedido.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    internal class Pedido
+    {
+        public List<Producto> Productos { get; set; }
+
+        public Pedido()
+        {
+            Productos = new List<Producto>();
+        }
+
+        public void AgregarProducto(Producto producto)
+        {
+            Productos.Add(producto);
+        }
+
+        public void EliminarProducto(Producto producto)
+        {
+            Productos.Remove(producto);
+        }
+
+        public double CalcularTotal()
+        {
+            return Productos.Sum(p => p.Valor);
+        }
+    }
+}
