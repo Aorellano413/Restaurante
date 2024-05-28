df6a597f6f7c228215f5b14b5f3ad226ae4a6cee
Santiago-Masco <santiagomasco2@gmail.com>
2024-05-28 16:39:16 -0500

    Agregacion de los datos del usuario en la capa Persistencia.

---
+++ Persistencia/DatosUsuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+
+namespace Persistencia
+{
+    public class DatosUsuario
+    {
+        private List<Usuario> usuarios = new List<Usuario>
+    {
+        new Usuario { NombreUsuario = "admin", Contraseña = "admin123", Rol = "Administrador" },
+        new Usuario { NombreUsuario = "cliente", Contraseña = "cliente123", Rol = "Cliente" }
+    };
+
+        public Usuario ObtenerUsuario(string nombreUsuario, string contraseña)
+        {
+            return usuarios.Find(u => u.NombreUsuario == nombreUsuario && u.Contraseña == contraseña);
+        }
+    }
+}
