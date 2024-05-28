3f97c7dfa90c8181042b3544323096bfff759ae5
Santiago-Masco <santiagomasco2@gmail.com>
2024-05-28 16:44:13 -0500

    Añadido el servicio del usuario.

--- Logica/ServicioUsuario.cs
+++ Logica/ServicioUsuario.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks; 
+using System.Threading.Tasks;
+using Persistencia;
+using Entidades;
 
 namespace Logica
 {
     public class ServicioUsuario 
     {
+        private DatosUsuario datosUsuario = new DatosUsuario();
 
+        public Usuario Autenticar(string nombreUsuario, string contraseña)
+        {
+            return datosUsuario.ObtenerUsuario(nombreUsuario, contraseña);
+        }
     }
 }
