d4f70229e9a166c56dda9d2f67b010148d0652c6
Andres Orellano <andresorellano591@gmail.com>
2024-05-29 19:27:36 -0500

    Nueva funcionalidad agregada en la vista

--- Vista/RegistroProductos.Designer.cs
+++ Vista/RegistroProductos.Designer.cs
@@ -44,41 +44,42 @@
             this.buttonGuardar = new System.Windows.Forms.Button();
             this.buttonBorrar = new System.Windows.Forms.Button();
             this.buttonNuevo2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.btnCerrarRegistroProducto = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // labelId
             // 
             this.labelId.AutoSize = true;
-            this.labelId.Location = new System.Drawing.Point(48, 49);
+            this.labelId.Location = new System.Drawing.Point(14, 95);
             this.labelId.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.labelId.Name = "labelId";
-            this.labelId.Size = new System.Drawing.Size(45, 29);
+            this.labelId.Size = new System.Drawing.Size(33, 20);
             this.labelId.TabIndex = 0;
             this.labelId.Text = "ID:";
             // 
             // labelPrecio
             // 
             this.labelPrecio.AutoSize = true;
-            this.labelPrecio.Location = new System.Drawing.Point(39, 163);
+            this.labelPrecio.Location = new System.Drawing.Point(14, 231);
             this.labelPrecio.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.labelPrecio.Name = "labelPrecio";
-            this.labelPrecio.Size = new System.Drawing.Size(117, 29);
+            this.labelPrecio.Size = new System.Drawing.Size(81, 20);
             this.labelPrecio.TabIndex = 1;
             this.labelPrecio.Text = "PRECIO:";
             // 
             // labelNombre
             // 
             this.labelNombre.AutoSize = true;
-            this.labelNombre.Location = new System.Drawing.Point(39, 108);
+            this.labelNombre.Location = new System.Drawing.Point(14, 163);
             this.labelNombre.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.labelNombre.Name = "labelNombre";
-            this.labelNombre.Size = new System.Drawing.Size(132, 29);
+            this.labelNombre.Size = new System.Drawing.Size(90, 20);
             this.labelNombre.TabIndex = 2;
             this.labelNombre.Text = "NOMBRE:";
             // 
@@ -87,10 +88,10 @@
             this.textId.BackColor = System.Drawing.Color.White;
             this.textId.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textId.Location = new System.Drawing.Point(180, 49);
+            this.textId.Location = new System.Drawing.Point(180, 95);
             this.textId.Margin = new System.Windows.Forms.Padding(5);
             this.textId.Name = "textId";
-            this.textId.Size = new System.Drawing.Size(164, 35);
+            this.textId.Size = new System.Drawing.Size(164, 26);
             this.textId.TabIndex = 3;
             // 
             // textPrecio
@@ -98,10 +99,10 @@
             this.textPrecio.BackColor = System.Drawing.Color.White;
             this.textPrecio.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textPrecio.Location = new System.Drawing.Point(180, 163);
+            this.textPrecio.Location = new System.Drawing.Point(180, 225);
             this.textPrecio.Margin = new System.Windows.Forms.Padding(5);
             this.textPrecio.Name = "textPrecio";
-            this.textPrecio.Size = new System.Drawing.Size(164, 35);
+            this.textPrecio.Size = new System.Drawing.Size(164, 26);
             this.textPrecio.TabIndex = 4;
             // 
             // textNombre
@@ -109,10 +110,10 @@
             this.textNombre.BackColor = System.Drawing.Color.White;
             this.textNombre.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textNombre.Location = new System.Drawing.Point(180, 105);
+            this.textNombre.Location = new System.Drawing.Point(180, 163);
             this.textNombre.Margin = new System.Windows.Forms.Padding(5);
             this.textNombre.Name = "textNombre";
-            this.textNombre.Size = new System.Drawing.Size(164, 35);
+            this.textNombre.Size = new System.Drawing.Size(164, 26);
             this.textNombre.TabIndex = 5;
             // 
             // dataGridView1
@@ -127,10 +128,10 @@
             this.FOTO});
             this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.dataGridView1.EnableHeadersVisualStyles = false;
-            this.dataGridView1.Location = new System.Drawing.Point(43, 197);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 265);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.Size = new System.Drawing.Size(402, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(663, 255);
             this.dataGridView1.TabIndex = 6;
             // 
             // ID
@@ -168,7 +169,7 @@
             this.buttonSeleccionar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonSeleccionar.Image = ((System.Drawing.Image)(resources.GetObject("buttonSeleccionar.Image")));
             this.buttonSeleccionar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonSeleccionar.Location = new System.Drawing.Point(516, 375);
+            this.buttonSeleccionar.Location = new System.Drawing.Point(870, 544);
             this.buttonSeleccionar.Name = "buttonSeleccionar";
             this.buttonSeleccionar.Size = new System.Drawing.Size(277, 44);
             this.buttonSeleccionar.TabIndex = 8;
@@ -183,7 +184,7 @@
             this.buttonGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonGuardar.Image = ((System.Drawing.Image)(resources.GetObject("buttonGuardar.Image")));
             this.buttonGuardar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonGuardar.Location = new System.Drawing.Point(196, 372);
+            this.buttonGuardar.Location = new System.Drawing.Point(257, 526);
             this.buttonGuardar.Name = "buttonGuardar";
             this.buttonGuardar.Size = new System.Drawing.Size(148, 50);
             this.buttonGuardar.TabIndex = 9;
@@ -199,7 +200,7 @@
             this.buttonBorrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonBorrar.Image = ((System.Drawing.Image)(resources.GetObject("buttonBorrar.Image")));
             this.buttonBorrar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonBorrar.Location = new System.Drawing.Point(369, 372);
+            this.buttonBorrar.Location = new System.Drawing.Point(548, 526);
             this.buttonBorrar.Name = "buttonBorrar";
             this.buttonBorrar.Size = new System.Drawing.Size(127, 50);
             this.buttonBorrar.TabIndex = 10;
@@ -215,7 +216,7 @@
             this.buttonNuevo2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonNuevo2.Image = ((System.Drawing.Image)(resources.GetObject("buttonNuevo2.Image")));
             this.buttonNuevo2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonNuevo2.Location = new System.Drawing.Point(43, 372);
+            this.buttonNuevo2.Location = new System.Drawing.Point(18, 526);
             this.buttonNuevo2.Name = "buttonNuevo2";
             this.buttonNuevo2.Size = new System.Drawing.Size(116, 50);
             this.buttonNuevo2.TabIndex = 11;
@@ -224,32 +225,22 @@
             this.buttonNuevo2.UseVisualStyleBackColor = true;
             this.buttonNuevo2.Click += new System.EventHandler(this.buttonNuevo2_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(564, 108);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(336, 40);
-            this.label1.TabIndex = 12;
-            this.label1.Text = "PRODUCTOS AJS";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(592, 69);
+            this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(459, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(275, 40);
+            this.label2.Size = new System.Drawing.Size(292, 35);
             this.label2.TabIndex = 13;
-            this.label2.Text = "REGISTRO DE";
+            this.label2.Text = "REGISTRO DE PRODUCTOS AJS";
             // 
             // btnCerrarRegistroProducto
             // 
             this.btnCerrarRegistroProducto.BackColor = System.Drawing.Color.Red;
             this.btnCerrarRegistroProducto.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnCerrarRegistroProducto.Image = ((System.Drawing.Image)(resources.GetObject("btnCerrarRegistroProducto.Image")));
-            this.btnCerrarRegistroProducto.Location = new System.Drawing.Point(897, 14);
+            this.btnCerrarRegistroProducto.Location = new System.Drawing.Point(1122, 5);
             this.btnCerrarRegistroProducto.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCerrarRegistroProducto.Name = "btnCerrarRegistroProducto";
             this.btnCerrarRegistroProducto.Size = new System.Drawing.Size(34, 35);
@@ -260,23 +251,32 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(549, 140);
+            this.pictureBox2.Location = new System.Drawing.Point(718, 121);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(264, 212);
+            this.pictureBox2.Size = new System.Drawing.Size(412, 360);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 15;
             this.pictureBox2.TabStop = false;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FloralWhite;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnCerrarRegistroProducto);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1160, 51);
+            this.panel1.TabIndex = 16;
+            // 
             // RegistroProductos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 29F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Khaki;
-            this.ClientSize = new System.Drawing.Size(944, 514);
+            this.ClientSize = new System.Drawing.Size(1160, 600);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.btnCerrarRegistroProducto);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonNuevo2);
             this.Controls.Add(this.buttonBorrar);
             this.Controls.Add(this.buttonGuardar);
@@ -295,6 +295,8 @@
             this.Text = "RegistroProductos";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -313,7 +315,6 @@
         private System.Windows.Forms.Button buttonGuardar;
         private System.Windows.Forms.Button buttonBorrar;
         private System.Windows.Forms.Button buttonNuevo2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnCerrarRegistroProducto;
         private System.Windows.Forms.DataGridViewTextBoxColumn ID;
@@ -321,5 +322,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn PRECIO;
         private System.Windows.Forms.DataGridViewTextBoxColumn FOTO;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
