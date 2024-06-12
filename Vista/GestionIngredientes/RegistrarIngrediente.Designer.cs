6f063fba435545782e3c43d112cc93b6f0debb8c
Andres Orellano <andresorellano591@gmail.com>
2024-06-11 22:16:43 -0500

    Nueva funcionalidad en la capa vista

--- Vista/GestionIngredientes/RegistrarIngrediente.Designer.cs
+++ Vista/GestionIngredientes/RegistrarIngrediente.Designer.cs
@@ -47,8 +47,9 @@
             this.panelRegistrarIngrediente.Controls.Add(this.btnCerrarRegistrarIngrediente);
             this.panelRegistrarIngrediente.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelRegistrarIngrediente.Location = new System.Drawing.Point(0, 0);
+            this.panelRegistrarIngrediente.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panelRegistrarIngrediente.Name = "panelRegistrarIngrediente";
-            this.panelRegistrarIngrediente.Size = new System.Drawing.Size(570, 51);
+            this.panelRegistrarIngrediente.Size = new System.Drawing.Size(380, 33);
             this.panelRegistrarIngrediente.TabIndex = 0;
             this.panelRegistrarIngrediente.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelRegistrarIngrediente_MouseDown);
             // 
@@ -56,9 +57,10 @@
             // 
             this.labelRegistrarIngrediente.AutoSize = true;
             this.labelRegistrarIngrediente.Font = new System.Drawing.Font("Bahnschrift Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelRegistrarIngrediente.Location = new System.Drawing.Point(110, 0);
+            this.labelRegistrarIngrediente.Location = new System.Drawing.Point(73, 0);
+            this.labelRegistrarIngrediente.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelRegistrarIngrediente.Name = "labelRegistrarIngrediente";
-            this.labelRegistrarIngrediente.Size = new System.Drawing.Size(305, 43);
+            this.labelRegistrarIngrediente.Size = new System.Drawing.Size(204, 29);
             this.labelRegistrarIngrediente.TabIndex = 16;
             this.labelRegistrarIngrediente.Text = "REGISTRAR INGREDIENTE";
             // 
@@ -69,10 +71,9 @@
             this.btnCerrarRegistrarIngrediente.FlatAppearance.BorderSize = 0;
             this.btnCerrarRegistrarIngrediente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCerrarRegistrarIngrediente.Image = ((System.Drawing.Image)(resources.GetObject("btnCerrarRegistrarIngrediente.Image")));
-            this.btnCerrarRegistrarIngrediente.Location = new System.Drawing.Point(518, 5);
-            this.btnCerrarRegistrarIngrediente.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCerrarRegistrarIngrediente.Location = new System.Drawing.Point(345, 3);
             this.btnCerrarRegistrarIngrediente.Name = "btnCerrarRegistrarIngrediente";
-            this.btnCerrarRegistrarIngrediente.Size = new System.Drawing.Size(34, 35);
+            this.btnCerrarRegistrarIngrediente.Size = new System.Drawing.Size(23, 23);
             this.btnCerrarRegistrarIngrediente.TabIndex = 15;
             this.btnCerrarRegistrarIngrediente.UseVisualStyleBackColor = false;
             this.btnCerrarRegistrarIngrediente.Click += new System.EventHandler(this.btnCerrarRegistrarIngrediente_Click);
@@ -81,9 +82,10 @@
             // 
             this.labelNombreRI.AutoSize = true;
             this.labelNombreRI.Font = new System.Drawing.Font("Bahnschrift Condensed", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelNombreRI.Location = new System.Drawing.Point(60, 97);
+            this.labelNombreRI.Location = new System.Drawing.Point(40, 63);
+            this.labelNombreRI.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelNombreRI.Name = "labelNombreRI";
-            this.labelNombreRI.Size = new System.Drawing.Size(103, 34);
+            this.labelNombreRI.Size = new System.Drawing.Size(69, 23);
             this.labelNombreRI.TabIndex = 1;
             this.labelNombreRI.Text = "NOMBRE :";
             // 
@@ -91,24 +93,27 @@
             // 
             this.labelStockRI.AutoSize = true;
             this.labelStockRI.Font = new System.Drawing.Font("Bahnschrift Condensed", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelStockRI.Location = new System.Drawing.Point(60, 157);
+            this.labelStockRI.Location = new System.Drawing.Point(40, 102);
+            this.labelStockRI.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelStockRI.Name = "labelStockRI";
-            this.labelStockRI.Size = new System.Drawing.Size(102, 34);
+            this.labelStockRI.Size = new System.Drawing.Size(69, 23);
             this.labelStockRI.TabIndex = 2;
             this.labelStockRI.Text = "STOCK    :";
             // 
             // txtNombreRI
             // 
-            this.txtNombreRI.Location = new System.Drawing.Point(170, 106);
+            this.txtNombreRI.Location = new System.Drawing.Point(113, 69);
+            this.txtNombreRI.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtNombreRI.Name = "txtNombreRI";
-            this.txtNombreRI.Size = new System.Drawing.Size(210, 26);
+            this.txtNombreRI.Size = new System.Drawing.Size(141, 20);
             this.txtNombreRI.TabIndex = 3;
             // 
             // txtStockRI
             // 
-            this.txtStockRI.Location = new System.Drawing.Point(170, 166);
+            this.txtStockRI.Location = new System.Drawing.Point(113, 108);
+            this.txtStockRI.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtStockRI.Name = "txtStockRI";
-            this.txtStockRI.Size = new System.Drawing.Size(210, 26);
+            this.txtStockRI.Size = new System.Drawing.Size(141, 20);
             this.txtStockRI.TabIndex = 4;
             // 
             // btnRegistrarIngrediente
@@ -120,9 +125,10 @@
             this.btnRegistrarIngrediente.ForeColor = System.Drawing.Color.Black;
             this.btnRegistrarIngrediente.Image = ((System.Drawing.Image)(resources.GetObject("btnRegistrarIngrediente.Image")));
             this.btnRegistrarIngrediente.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnRegistrarIngrediente.Location = new System.Drawing.Point(138, 226);
+            this.btnRegistrarIngrediente.Location = new System.Drawing.Point(92, 146);
+            this.btnRegistrarIngrediente.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnRegistrarIngrediente.Name = "btnRegistrarIngrediente";
-            this.btnRegistrarIngrediente.Size = new System.Drawing.Size(243, 132);
+            this.btnRegistrarIngrediente.Size = new System.Drawing.Size(162, 86);
             this.btnRegistrarIngrediente.TabIndex = 5;
             this.btnRegistrarIngrediente.Text = "REGISTRAR";
             this.btnRegistrarIngrediente.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -131,10 +137,10 @@
             // 
             // RegistrarIngrediente
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Snow;
-            this.ClientSize = new System.Drawing.Size(570, 414);
+            this.ClientSize = new System.Drawing.Size(380, 269);
             this.Controls.Add(this.btnRegistrarIngrediente);
             this.Controls.Add(this.txtStockRI);
             this.Controls.Add(this.txtNombreRI);
@@ -142,6 +148,7 @@
             this.Controls.Add(this.labelNombreRI);
             this.Controls.Add(this.panelRegistrarIngrediente);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "RegistrarIngrediente";
             this.Text = "RegistrarIngrediente";
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.RegistrarIngrediente_MouseDown);
